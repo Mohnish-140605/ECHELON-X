0a165f30d345f0f385cb58d64955021468488057
Yakari2268 <reqential@gmail.com>
2025-02-15 03:04:13 +0530

    Sleep detection

--- backend/gaze.py
+++ backend/gaze.py
@@ -3,6 +3,46 @@ import cv2
 import numpy as np
 from mediapipe.framework.formats.landmark_pb2 import NormalizedLandmark, NormalizedLandmarkList
 from numpy import ndarray
+from scipy.spatial import distance
+from imutils import face_utils
+import imutils
+import dlib
+
+
+def eye_aspect_ratio(eye):
+	A = distance.euclidean(eye[1], eye[5])
+	B = distance.euclidean(eye[2], eye[4])
+	C = distance.euclidean(eye[0], eye[3])
+	ear = (A + B) / (2.0 * C)
+	return ear
+
+def sleep_detection(frame:ndarray) -> str:
+    thresh = 0.25
+    frame_check = 20
+    detect = dlib.get_frontal_face_detector()
+    predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")# Dat file is the crux of the code
+
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    subjects = detect(gray, 0)
+    for subject in subjects:
+        shape = predict(gray, subject)
+        shape = face_utils.shape_to_np(shape)#converting to NumPy Array
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        ear = (leftEAR + rightEAR) / 2.0
+        if ear < thresh:
+            flag += 1
+            print (flag)
+            if flag >= frame_check:
+                return "sleepy"
+        else:
+            flag = 0
+
 
 def relative(landmark: NormalizedLandmark, shape: tuple[int, int, int]) -> tuple[int, int]:
     return (int(landmark.x * shape[1]), int(landmark.y * shape[0]))
@@ -108,6 +148,8 @@ def gaze(frame: ndarray, points: NormalizedLandmarkList) -> str:
             return "down"
 
         elif 10 > points_diff[0] > -40:
+
+
             return "forward"
 
         elif points_diff[0] != abs_points_diff[0]:
